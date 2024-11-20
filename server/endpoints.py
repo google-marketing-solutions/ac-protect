6ce048f4014145ef3f31ff6b18e5622913b6ca74
Ido Shalit <idoshalit@google.com>
2024-11-21 00:30:44 +0200

    Updated server endpoints
    Change-Id: Ia6bff8c67aeb63eb6d16aa771bd4421cd7e06756

--- server/endpoints.py
+++ server/endpoints.py
@@ -1,4 +1,4 @@
-# Copyright 2023 Google LLC
+# Copyright 2024 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,33 +11,29 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Endpoints that are triggered by the UI"""
-from typing import Dict
-from typing import List
+"""Module that provides endpoints to connect the frontend and backend services.
+
+This module contains endpoint functions that serve as the interface between the
+frontend UI and the backend services.
+"""
+
+# pylint: disable=C0330, g-multiple-import
+from typing import Any
 
 import pandas as pd
 
-from env import CONFIG_FILE_PATH
-from server.collectors.gads import GAdsCollector
-from server.config import get_config
-from server.config import update_config
-from server.db.bq import BigQuery
-from server.db.bq import BigQueryException
-from server.db.tables import GADS_TABLE_NAME
+from server import config, env
+from server.collectors import gads
+from server.db import bq, tables
 
-def run_gads_collector(auth, collector_config, bq):
-  """Pull, process and save data from Google Ads"""
-  collector = GAdsCollector(auth, collector_config, bq)
-  data = collector.collect()
-  data = collector.process(data)
-  collector.save(data, False)
-  return data
 
-def get_ads_data(auth: Dict,
-                 collector_config: Dict,
-                 bq_config: Dict,
-                 cols=None) -> pd.DataFrame:
-  """Get Google Ads Data From BigQuery table
+def get_ads_data_endpoint(
+  auth: dict[str, str],
+  collector_config: dict[str, Any],
+  bq_config: dict[str, Any],
+  cols: list[str] | None = None,
+) -> pd.DataFrame:
+  """Endpoint to fetch Google Ads Data From BigQuery table.
 
   Args:
     auth: auth configuration
@@ -45,60 +41,30 @@ def get_ads_data(auth: Dict,
     bq_config: BigQuery configuration
     cols: specific columns we want returned. Defaults to '*', returning all
       columns.
+
   Returns:
     A DataFrame with the values returned from the table. Returns None if there
       are no results.
   """
-  bq = BigQuery(auth, bq_config)
-  bq.connect()
+  db = bq.BigQuery(auth, bq_config)
+  db.connect()
 
   if not cols:
     cols = ['*']
 
   try:
-    ads_data = bq.get_values_from_table(GADS_TABLE_NAME, cols)
+    ads_data = db.get_values_from_table(tables.GADS_TABLE_NAME, cols)
     if ads_data.empty:
-      ads_data = run_gads_collector(auth, collector_config, bq)
+      return _run_gads_collector(auth, collector_config, db)
     return ads_data
-  except BigQueryException:
-    return run_gads_collector(auth, collector_config, bq)
+  except bq.BigQueryException:
+    return _run_gads_collector(auth, collector_config, db)
 
 
-
-def get_ads_app_ids(auth: Dict, collector_config: Dict,
-                    bq_config: Dict) -> List[str]:
-  """Get all app_ids from the Google Ads Table
-
-  Args:
-    auth: auth configuration
-    collector_config: collector configuration
-    bq_config: BigQuery configuration
-
-  Returns:
-    A list of app_ids
-  """
-  ads_data = get_ads_data(auth, collector_config, bq_config, ['app_id'])
-  return ads_data['app_id'].unique().tolist()
-
-
-def get_ads_event_names(auth: Dict, collector_config: Dict,
-                    bq_config: Dict) -> List[str]:
-  """Get all event_names from the Google Ads Table
-
-  Args:
-    auth: auth configuration
-    collector_config: collector configuration
-    bq_config: BigQuery configuration
-
-  Returns:
-    A list of event_names
-  """
-  ads_data = get_ads_data(auth, collector_config, bq_config, ['event_name'])
-  return ads_data['event_name'].unique().tolist()
-
-
-def get_config_file(config_path: str=CONFIG_FILE_PATH) -> Dict:
-  """Get the config file
+def get_config_endpoint(
+  config_path: str = env.CONFIG_FILE_PATH,
+) -> dict[str, Any]:
+  """Fetch the config file.
 
   Args:
     config_path: path to the config file. Defaults to CONFIG_FILE_PATH from
@@ -107,16 +73,37 @@ def get_config_file(config_path: str=CONFIG_FILE_PATH) -> Dict:
   Returns:
     Dictionary of config
   """
-  config = get_config(config_path)
-  return config
+  return config.get_config(config_path)
 
 
-def update_config_file(config: Dict, config_path: str = CONFIG_FILE_PATH):
-  """Update the config file
+def update_config_endpoint(
+  new_config: dict[str, Any], config_path: str = env.CONFIG_FILE_PATH
+) -> None:
+  """Update the config file.
 
   Args:
-    config: The new configuration we want to update
+    new_config: The new configuration we want to update
     config_path: path to the config file. Defaults to CONFIG_FILE_PATH from
     environment
   """
-  update_config(config, config_path)
+  config.update_config(new_config, config_path)
+
+
+def _run_gads_collector(
+  auth: dict[str, str], collector_config: dict[str, Any], db: bq.BigQuery
+) -> pd.DataFrame:
+  """Fetch, process and save data from Google Ads.
+
+  Args:
+    auth: auth configuration
+    collector_config: collector configuration
+    db: BigQuery object
+
+  Returns:
+    pd.DataFrame: DataFrame containing the fetched ads data.
+  """
+  collector = gads.GAdsCollector(auth, collector_config, db)
+  data = collector.collect()
+  data = collector.process(data)
+  collector.save(data, False)
+  return data
