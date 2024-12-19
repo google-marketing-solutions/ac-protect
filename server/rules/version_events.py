44b84414008e5bdaaf186048f0e468bad76a0381
Ido Shalit <idoshalit@google.com>
2024-12-19 11:03:25 +0200

    Fix TZ bug in version_events
    Changed store_timestamp and event_timestamp to UTC by default.
    
    Change-Id: I6d549a2dc74d34f970d5aa0887d47607da4e2031

--- server/rules/version_events.py
+++ server/rules/version_events.py
@@ -505,10 +505,8 @@ class VersionEventsRule(rules.Rule):
           .reset_index(drop=True)
         )
 
-        store_timestamp = pd.to_datetime(store_first['timestamp'][0])
-        store_timestamp.tz_convert(None)
-        event_timestamp = pd.to_datetime(event_first['date_added'][0])
-        event_timestamp.tz_convert(None)
+        store_timestamp = pd.to_datetime(store_first['timestamp'][0], utc=True)
+        event_timestamp = pd.to_datetime(event_first['date_added'][0], utc=True)
 
         if store_timestamp > event_timestamp:
           return store_ver
